36a261764d3012028baea5692a1485eefd058749
EdgarRetes <edgar.retes24@gmail.com>
2025-10-01 22:55:50 -0600

    vercel

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,8 @@ async function bootstrap() {
 
   // Enable CORS for frontend
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://polaris-frontend-theta.vercel.app'], // frontend URL
+    origin: '*',
+    // origin: ['http://localhost:5173', 'https://polaris-frontend-theta.vercel.app'], // frontend URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     // credentials: true,
   });
