53635f12462f651758d889ad8471d0137914a432
EdgarRetes <edgar.retes24@gmail.com>
2025-10-08 23:50:06 -0600

    vercel

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://polaris-frontend-theta.vercel.app'],
+    origin: 'https://polaris-frontend-theta.vercel.app',
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     credentials: true,
     allowedHeaders: [
