ede1ea01f6eb23079fbdd07211305d251ad27e5e
EdgarRetes <edgar.retes24@gmail.com>
2025-10-21 12:52:26 -0600

    token

--- src/audit_logs/audit.controller.ts
+++ src/audit_logs/audit.controller.ts
@@ -1,17 +1,15 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuditService } from '../audit_logs/audit.service';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-import { RolesGuard } from '../auth/guards/roles.guard';
-import { Roles } from '../auth/guards/roles.decorator';
+// import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+// import { RolesGuard } from '../auth/guards/roles.guard';
+// import { Roles } from '../auth/guards/roles.decorator';
 
 @Controller('audit')
-@UseGuards(JwtAuthGuard, RolesGuard)
+// @UseGuards(JwtAuthGuard)
 export class AuditController {
   constructor(private readonly audit: AuditService) {}
 
-  // Solo los Admin pueden consultar auditorías
   @Get()
-  @Roles('Admin')
   findAll() {
     return this.audit.findAll();
   }
